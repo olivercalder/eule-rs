6cd05a56f8adf6652cd7a58b79080c8eaf31c4ad
Oliver Calder <oliver.calder@canonical.com>
2023-04-10 12:50:58 -0500

    Added unit tests and safe function which cleans duplicates and multiples first.
    Signed-off-by: Oliver Calder <oliver.calder@canonical.com>

--- p001/src/main.rs
+++ p001/src/main.rs
@@ -7,15 +7,71 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 use combinatorial::Combinations;
 
-fn main() {
-    let limit: i64 = 1000;
-    let numbers: Vec<i64> = vec![3, 5];
-    let total = Combinations::all(numbers).skip(1).fold(0, |acc, comb| {
+/// Computes the sum of all multiples of the given numbers below the specified limit.
+///
+/// Assumes that `numbers` does not contain duplicate entries and that no entry is a multiple of
+/// any other entry.
+fn sum_of_multiples(numbers: Vec<i64>, limit: i64) -> i64 {
+    Combinations::all(numbers).skip(1).fold(0, |acc, comb| {
         let product: i64 = comb.iter().product();
         let count = (limit - 1) / product;
         let sum = count * (count + 1) / 2;
         let value = sum * product;
-        acc + (-1 + 2 * (comb.len() as i64 % 2)) * value
-    });
+        acc + (-1 + 2 * (comb.len() % 2) as i64) * value
+    })
+}
+
+/// Removes duplicate numbers and entries which are multiples of another entry.
+fn remove_duplicates_multiples(numbers: Vec<i64>) -> Vec<i64> {
+    let mut clean = numbers.clone();
+    clean.sort_unstable();
+    clean.dedup();
+    clean.iter().fold(Vec::new(), |mut acc, number| {
+        if !acc.iter().any(|x| number % x == 0) {
+            acc.push(*number);
+        }
+        acc
+    })
+}
+
+/// Computes the sum of all multiples of the given numbers below the specified limit.
+///
+/// Computes the correct answer regardless of whether `numbers` contains duplicate entries or
+/// entries which are multiples of any other entry.
+fn sum_of_multiples_safe(numbers: Vec<i64>, limit: i64) -> i64 {
+    let clean = remove_duplicates_multiples(numbers);
+    sum_of_multiples(clean, limit)
+}
+
+fn main() {
+    let numbers: Vec<i64> = vec![3, 5];
+    let limit: i64 = 1000;
+    let total = sum_of_multiples(numbers, limit);
     println!("{}", total);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solution() {
+        let numbers = vec![3, 5];
+        let limit = 1000;
+        assert_eq!(sum_of_multiples(numbers, limit), 233168);
+    }
+
+    #[test]
+    fn test_more_numbers() {
+        let numbers = vec![2, 3, 5, 7];
+        let limit = 1000;
+        assert_eq!(sum_of_multiples(numbers, limit), 385788);
+    }
+
+    #[test]
+    fn test_numbers_with_multiples() {
+        let numbers = vec![3, 5, 6];
+        let limit = 1000;
+        assert_eq!(sum_of_multiples_safe(numbers, limit), 233168);
+    }
 }
